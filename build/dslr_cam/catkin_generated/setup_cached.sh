9040f6f5ac932e2ca42657968e3e45ec0ee71cdf
Ammar@NUC3 <ammarajml@gmail.com>
2024-03-13 00:56:41 +0900

    updated the sony cam node.

--- build/dslr_cam/catkin_generated/setup_cached.sh
+++ build/dslr_cam/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/sitl3/sitl_ws/devel/.private/dslr_cam:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/sitl3/sitl_ws/devel/.private/dslr_cam/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/sitl3/sitl_ws/devel/.private/dslr_cam/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/sitl3/sitl_ws/build/dslr_cam'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/sitl3/sitl_ws/devel/.private/dslr_cam/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/sitl3/sitl_ws/src/dslr_cam:$ROS_PACKAGE_PATH"
