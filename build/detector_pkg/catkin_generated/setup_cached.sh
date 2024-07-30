c2243ba85d3c22640e6fe2d4853efaadbe6a9543
Ammar Ajmal <ammarajml@gmial.com>
2024-07-31 01:11:02 +0900

    detection

--- build/detector_pkg/catkin_generated/setup_cached.sh
+++ build/detector_pkg/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/tesol/sitl_ws/devel/.private/detector_pkg:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/tesol/sitl_ws/devel/.private/detector_pkg/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/tesol/sitl_ws/devel/.private/detector_pkg/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/tesol/sitl_ws/build/detector_pkg'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/tesol/sitl_ws/devel/.private/detector_pkg/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/tesol/sitl_ws/src/detector_pkg:$ROS_PACKAGE_PATH"
